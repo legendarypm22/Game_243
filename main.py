c6db53c1e9cc5af4155a59d8fe3a5950f4940c6c
Legendarypm22 <marvelravenclaw@hotmail.com>
2020-12-30 20:17:58 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("welcome to my first game")
+name = input("what is your name:")
+age = input("What is your age:")
+
+health = 10
+
+
+if int(age) >= 12:
+    print("you are old enough to play")
+else:
+    print("you are not old enough to play")
+    print("Lets get started")
+    input("Which do you choose left or right left/right?")
+if input == "left":
+    print("you went the correct way and passed")
